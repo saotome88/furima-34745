42adfb094a09f7bf32ba30484b39fa40dd70775f
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-05 10:22:12 +0900

    items/newアクション/ViewファイルとCSSファイルの設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   private
   def items_params
     params.require(:items).permit(:name, :descriptionm, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
