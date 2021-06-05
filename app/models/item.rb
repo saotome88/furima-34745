4b13c9d811a359dd4cb400e37b96821d713d5f29
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-05 10:11:48 +0900

    ActiveStorageのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, :descriptionm, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :price
   end
-
+  
+  has_one_attached :image
   belongs_to :user
 #  has_one :purchase
 end
