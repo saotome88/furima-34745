01039e64b35708f1c5dcb091cec48de99d67f9c7
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-07 15:30:50 +0900

    出品-商品状態の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  # ActiveHashで保存されたデータ項目
   belongs_to :category
-  validates :category_id, numericality: { other_than: 1 }
+  belongs_to :condition
 
+  # ActiveHashでid:1の"---"で保存されないように設定
+  with_options numericality:  { other_than: 1 }
+    validates :category_id, :condition_id
+  end
+
+  # 空の値は保存されないようにする。
   with_options presence: true do
-    validates :name, :descriptionm, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :image
+  validates :name, :descriptionm, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :image
   end
   validates :price, presence: true, format: {with: /\A[0-9]+\z/}, numericality: {only_integer: true, greather_than: 300, less_than: 9999999}
 
