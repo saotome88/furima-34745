ee0626540d1393d7867f7556bdc7d8fa7387c47c
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-08 00:51:13 +0900

    出品-販売価格バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   with_options presence: true do
   validates :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :image
   end
-  validates :price, presence: true, format: {with: /\A[0-9]+\z/}, numericality: {only_integer: true, greather_than: 300, less_than: 9999999}
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/}, numericality: {only_integer: true, greater_than: 300, less_than: 9999999}
 
   belongs_to :user
   has_one_attached :image
