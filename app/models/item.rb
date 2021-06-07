8ed95a0c19ed44fcf2a876cc8d51051924fa438a
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-07 15:23:08 +0900

    出品-カテゴリー選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 }
+
   with_options presence: true do
     validates :name, :descriptionm, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :image
   end
   validates :price, presence: true, format: {with: /\A[0-9]+\z/}, numericality: {only_integer: true, greather_than: 300, less_than: 9999999}
 
-  has_one_attached :image
   belongs_to :user
-#  has_one :purchase
+  has_one_attached :image
+# has_one :purchase
 end
