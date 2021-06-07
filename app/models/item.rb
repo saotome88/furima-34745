27f360065a376de447280c93caa58550f15d7ad0
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-07 15:45:49 +0900

    出品-配送日数の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,11 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :prefecture
+  belongs_to :shipping_day
 
   # ActiveHashでid:1の"---"で保存されないように設定
   with_options numericality:  { other_than: 1 }
-    validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id
+    validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id
   end
 
   # 空の値は保存されないようにする。
