443ad19c8bf5c95d9095208ff30d301838601959
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-08 00:50:37 +0900

    出品-販売価格表示のJS実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,18 @@
+function inputPrice() {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+
+    const price = itemPrice.value
+    const tax = Math.floor(price * 0.1)
+    const profit = Math.floor(price - tax)
+
+    console.log(price);
+    const taxPrice = document.getElementById("add-tax-price");
+    const profitPrice = document.getElementById("profit");
+    taxPrice.innerHTML = tax;
+    profitPrice.innerHTML = profit;
+  })
+};
+
+/* 全てのページで発火するが、functionの中身でkeyupを指定しているから良しとする。 */
+window.addEventListener('load', inputPrice);
