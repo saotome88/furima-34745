be2b1f1b2271d7fa4ad37c0f20aedb270b29c485
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-21 11:29:20 +0900

    購入機能-PurchaseBuyerで必要ない記述を削除

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -19,9 +19,9 @@ class PurchaseBuyer
   def save
     # 変数purchaseにuser_idとitem_idを代入する
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    # 変数buyerに購入者の情報を代入する
-    buyer = Buyer.create(purchase_id: purchase.id, shipping_postal_code: shipping_postal_code, prefecture_id: prefecture_id,
-    shipping_address_municipality: shipping_address_municipality, shipping_address_number: shipping_address_number,
-    shipping_address_building: shipping_address_building, shipping_phone_number: shipping_phone_number)
+    # 購入者の情報を保存する
+    Buyer.create(purchase_id: purchase.id, shipping_postal_code: shipping_postal_code, prefecture_id: prefecture_id,
+                 shipping_address_municipality: shipping_address_municipality, shipping_address_number: shipping_address_number,
+                 shipping_address_building: shipping_address_building, shipping_phone_number: shipping_phone_number)
   end
 end
