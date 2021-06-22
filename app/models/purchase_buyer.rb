5382cfe4ee88f7a4c6ada96a3b6ee0f938b49f94
saotome88 <ryoji.saotome1988@gmail.com>
2021-06-22 17:31:14 +0900

    購入機能実装後にrubocopを実行

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -1,15 +1,16 @@
 class PurchaseBuyer
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :shipping_postal_code, :prefecture_id, :shipping_address_municipality,:shipping_address_number, :shipping_address_building, :shipping_phone_number, :token
+  attr_accessor :user_id, :item_id, :shipping_postal_code, :prefecture_id, :shipping_address_municipality,
+                :shipping_address_number, :shipping_address_building, :shipping_phone_number, :token
 
   with_options presence: true do
     # アソシエーションを設定できないのでuser_idとitem_idもバリデーションに加える
     validates :user_id, :item_id, :shipping_address_municipality,
               :shipping_address_number, :token
     # 郵便番号はハイフン込みの半角数字で123-1234の桁数に指定する
-    validates :shipping_postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :shipping_postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     # 電話番号は半角数字のみにする
-    validates :shipping_phone_number, format: {with: /\A[0-9]{10,11}\z/}
+    validates :shipping_phone_number, format: { with: /\A[0-9]{10,11}\z/ }
   end
   # 都道府県選択はActivehashなので、0以外の選択させる
   validates :prefecture_id, numericality: { other_than: 0 }
--- spec/models/purchase_buyer_spec.rb
+++ spec/models/purchase_buyer_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe PurchaseBuyer, type: :model do
         expect(@purchase_buyer).to be_valid
       end
       it '配送先の建物名が空でも購入ができる' do
-        @purchase_buyer.shipping_address_building = ""
+        @purchase_buyer.shipping_address_building = ''
         expect(@purchase_buyer).to be_valid
       end
       it '配送先の電話番号は10桁でも購入ができる' do
-        @purchase_buyer.shipping_phone_number = "1234567890"
+        @purchase_buyer.shipping_phone_number = '1234567890'
         expect(@purchase_buyer).to be_valid
       end
     end
@@ -28,82 +28,82 @@ RSpec.describe PurchaseBuyer, type: :model do
       it 'クレジットカード情報が入力されていないと購入できない' do
         @purchase_buyer.token = nil
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("クレジットカード情報を入力してください")
+        expect(@purchase_buyer.errors.full_messages).to include('クレジットカード情報を入力してください')
       end
       it '配送先で郵便番号が空だと購入ができない' do
-        @purchase_buyer.shipping_postal_code = ""
+        @purchase_buyer.shipping_postal_code = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('郵便番号を入力してください', '郵便番号は不正な値です')
       end
       it '配送先で郵便番号が全角入力だと購入ができない' do
-        @purchase_buyer.shipping_postal_code = "１２３ー１２３４"
+        @purchase_buyer.shipping_postal_code = '１２３ー１２３４'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("郵便番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('郵便番号は不正な値です')
       end
       it '配送先で郵便番号でハイフンがないと購入ができない' do
-        @purchase_buyer.shipping_postal_code = "1231234"
+        @purchase_buyer.shipping_postal_code = '1231234'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("郵便番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('郵便番号は不正な値です')
       end
       it '配送先でハイフンの前が3桁でないと購入ができない' do
-        @purchase_buyer.shipping_postal_code = "1234-1234"
+        @purchase_buyer.shipping_postal_code = '1234-1234'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("郵便番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('郵便番号は不正な値です')
       end
       it '配送先で郵便番号でハイフンの後が4桁でないと購入ができない' do
-        @purchase_buyer.shipping_postal_code = "123-12345"
+        @purchase_buyer.shipping_postal_code = '123-12345'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("郵便番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('郵便番号は不正な値です')
       end
       it '配送先で都道府県が選択されていないと購入ができない' do
         @purchase_buyer.prefecture_id = 0
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("都道府県は0以外の値にしてください")
+        expect(@purchase_buyer.errors.full_messages).to include('都道府県は0以外の値にしてください')
       end
       it '配送先で市区町村が空だと購入ができない' do
-        @purchase_buyer.shipping_address_municipality = ""
+        @purchase_buyer.shipping_address_municipality = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("市区町村を入力してください")
+        expect(@purchase_buyer.errors.full_messages).to include('市区町村を入力してください')
       end
       it '配送先で番地が空だと購入ができない' do
-        @purchase_buyer.shipping_address_number = ""
+        @purchase_buyer.shipping_address_number = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("番地を入力してください")
+        expect(@purchase_buyer.errors.full_messages).to include('番地を入力してください')
       end
       it '配送先で電話番号が空だと購入ができない' do
-        @purchase_buyer.shipping_phone_number = ""
+        @purchase_buyer.shipping_phone_number = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("電話番号を入力してください", "電話番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('電話番号を入力してください', '電話番号は不正な値です')
       end
       it '配送先で電話番号に全角入力だと購入ができない' do
-        @purchase_buyer.shipping_phone_number = "１２３１２３４１２３４"
+        @purchase_buyer.shipping_phone_number = '１２３１２３４１２３４'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('電話番号は不正な値です')
       end
       it '配送先で電話番号に半角数字以外の入力があると購入ができない' do
-        @purchase_buyer.shipping_phone_number = "123-123-123"
+        @purchase_buyer.shipping_phone_number = '123-123-123'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('電話番号は不正な値です')
       end
       it '配送先で電話番号に11桁より多い入力だと購入ができない' do
-        @purchase_buyer.shipping_phone_number = "123456789012"
+        @purchase_buyer.shipping_phone_number = '123456789012'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('電話番号は不正な値です')
       end
       it '配送先で電話番号に10桁より少ない入力だと購入ができない' do
-        @purchase_buyer.shipping_phone_number = "123456789"
+        @purchase_buyer.shipping_phone_number = '123456789'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
+        expect(@purchase_buyer.errors.full_messages).to include('電話番号は不正な値です')
       end
       it '購入商品のitem_idが紐づいていないと購入できないこと' do
         @purchase_buyer.item_id = nil
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("Itemを入力してください")
+        expect(@purchase_buyer.errors.full_messages).to include('Itemを入力してください')
       end
       it '購入者のuser_idが紐づいていないと購入できないこと' do
         @purchase_buyer.user_id = nil
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("Userを入力してください")
+        expect(@purchase_buyer.errors.full_messages).to include('Userを入力してください')
       end
     end
   end
